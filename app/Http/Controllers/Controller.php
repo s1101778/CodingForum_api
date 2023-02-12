397a3987e7d3f271870a24ed50ee2c1d38a6785f
Colten <likeyou600@gmail.com>
2023-02-13 01:51:59 +0800

    Feat : 添加基本功能

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Mail;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sendmail()
+    {
+        Mail::raw('測試郵件', function ($message) {
+            $message->from('root@bakerychu.com', 'root');
+            $message->to('likeyou600@gmail.com', 'bakery')->subject('網域發信');
+        });
+    }
 }
