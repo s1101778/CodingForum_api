82fea81fc96a601b12fe0e95506436601d211eac
Colten <likeyou600@gmail.com>
2023-03-01 02:40:55 +0800

    Stop : 停止cursorPaginate的研究 改用其他方法

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,26 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\User;
 use App\Models\Post;
-use App\Models\UserLike;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
 
+    public function showPage()
+    {
+        // $comments = Comment::select('*')->orderBy('id', 'ASC')->cursorPaginate(5);
+
+        $comments = Comment::orderByDesc('likes')->orderByDesc('id')->cursorPaginate(5);
+        return view('welcome', ['comments' => $comments]);
+    }
+
     public function test1()
     {
-        $a = UserLike::where([ //ig愛心格式才需要
-            'user_id' => 1,
-            'post_id' => 1
-        ])->whereNull('comment_id');
-        if ($a->exists()) {
-            return response()->json(['success' =>  $a], 200);
-        }
+        // orderBy('likes') where('post_id', 1)->whereNull('parent_comment_id')->orderBy('created_at', 'DESC')->where('com.post_id', 1)->
+        // $comment = DB::table('comments')->select("select * from comments as com order by 'com.likes' desc")->cursorPaginate(5);
+        // $comment = \DB::select("select * from comments as comments order by 'comments.likes' desc")->cursorPaginate(5);
+
+
+        $comment = Comment::orderByDesc('likes')->orderBy('created_at')->orderByDesc('id')->cursorPaginate(5);
+
+
+        // $next_cursor = $comment->toArray()['next_cursor'];
+
+        // $comment = self::tidy_comment($comment, $next_cursor);
+
+        return response()->json(['success' =>  $comment], 200);
     }
+
     public function test2()
     {
+        $children_comments = Comment::where('parent_comment_id', 2)->orderBy('likes', 'DESC')->orderBy('created_at', 'DESC')->cursorPaginate(2);
+        $next_cursor = $children_comments->toArray()['next_cursor'];
 
-        return response()->json(['success' => 'success'], 200);
+        $children_comments = self::tidy_comment($children_comments, $next_cursor);
+
+        return response()->json(['success' => $children_comments], 200);
     }
     public function test3()
     {
@@ -30,5 +51,34 @@ class TestController extends Controller
     public function test4()
     {
         return response()->json(['success' => 'success'], 200);
+    }
+    public function tidy_comment($comments, $next_cursor)
+    {
+        $comments = $comments->map(function ($item, $key) {
+            $mention = collect(json_decode($item['mention'], true));
+            $mention_name = collect();
+            $mention->map(function ($item, $key) use ($mention_name) {
+                $mention_name->push(User::find($item)->name);
+            });
+            return collect([
+                'id' => $item['id'],
+                'user_id' => $item['user_id'],
+                'post_id' => $item['post_id'],
+                'user_name' => $item->User->name,
+                'pic_url' => $item->User->pic_url,
+                'content' => $item['content'],
+                'likes' => $item['likes'],
+                'children_comment_count' => $item['children_comment_count'],
+                'children_comments' => [],
+                'mention' => $mention,
+                'mention_name' => $mention_name,
+                'created_at' => $item['created_at']->format('Y/m/d H:i:s'),
+                'updated_at' => $item['updated_at']->format('Y/m/d H:i:s'),
+            ]);
+        });
+        $out_comment = collect();
+        $out_comment->put('next_cursor', $next_cursor);
+        $out_comment->put('data', $comments);
+        return $out_comment;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('CodingForum', function () {
-    return view('welcome');
-});
+Route::get('CodingForum', [TestController::class, 'showPage']);
+
+// Route::get('CodingForum', function () {
+//     return view('welcome');
+// });
